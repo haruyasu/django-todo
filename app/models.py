02c54e3861771226c25a2feedf9b11e410c82368
haruyasu <harukun2002@gmail.com>
2020-11-16 17:09:38 +0900

    バックエンド追加

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+	title = models.CharField("タイトル", max_length=200)
+	deadline = models.DateField("期限")
+
+	def __str__(self):
+		return self.title
